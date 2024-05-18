f2dc75a8ba9b8f17128509bc6e1ba71b853a1929
Ebenezer Joshua <ebenezerjoshua1410@gmail.com>
2024-05-18 16:43:06 +0530

    just a asimple edit

--- pages/about.vue
+++ pages/about.vue
@@ -6,7 +6,7 @@
         <p v-bind="source">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum quibusdam quaerat laudantium! Consequatur harum ratione veniam rem temporibus tenetur optio eum repellendus quo fuga. Facilis aspernatur similique quod natus labore!</p>
         <br>
         <NuxtLink to="/" style="color:#12b488"><b>Home Link</b></NuxtLink> <br/>
-        <a href="/">home page anchor</a>
+        <a href="/">home page anchor tag</a>
     </div>
 </template>
 
